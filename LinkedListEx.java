d854fd6bf9b7029376fcbceb7cf7038816a4e101
rajasekhar1404 <mrrajasekhar09@gmail.com>
2021-12-10 10:33:47 +0530

    linkedlist example

---
+++ LinkedListEx.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class LinkedListEx {
+    public static void main(String[] args)
+    {
+        LinkedHashSet<String> names = new LinkedHashSet<String>();
+
+        names.add("rajasekhar");
+        names.add("karampudi");
+        names.add("aryan");
+        names.add("rajkumar");
+
+        System.out.println(names);
+    }
+    
+}
