e23dd3c4c495501f1528189e777af54fb0ec18c4
rajasekhar1404 <mrrajasekhar09@gmail.com>
2021-12-19 13:26:05 +0530

    imp classes

---
+++ DateEx.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class DateEx {
+    public static void main(String[] args)
+    {
+        Date a = new Date();
+        System.out.println(a);
+        System.out.println(a.getTime());
+    }    
+}
