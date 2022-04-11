f4f87913d1f241d0aff41a5591d9dab3f9229f33
rajasekhar1404 <mrrajasekhar09@gmail.com>
2022-04-11 22:50:37 +0530

    Solved java_End_of_file

--- Java_End_of_file.java
+++ Java_End_of_file.java
@@ -9,11 +9,11 @@ public class Java_End_of_file {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-
-        if (sc.hasNext())
+        int i = 1;
+        while (sc.hasNext())
         {
-            System.out.println();
+            System.out.println(i+ " "+sc.nextLine());
+            i++;
         }
     }
 }
