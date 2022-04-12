31566b70b3dda76da61a1e2e3287097efffb75ab
Rajasekhar <mrrajasekhar09@gmail.com>
2022-04-12 09:53:38 +0530

    Solved Java_Date_and_Time

--- Java_Date_and_Time.java
+++ Java_Date_and_Time.java
@@ -23,9 +23,11 @@ class Result {
      */
 
     public static String findDay(int month, int day, int year) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(month,day,year);
-        return calendar.getDisplayName(calendar.get(Calendar.DAY_OF_WEEK), Calendar.LONG, Locale.getDefault());
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, month-1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        cal.set(Calendar.YEAR, year);
+        return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
     }
 
 }
