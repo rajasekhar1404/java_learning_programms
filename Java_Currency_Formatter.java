394d1745956776691c1e2084a011d5693a33c10d
Rajasekhar <mrrajasekhar09@gmail.com>
2022-04-15 15:40:17 +0530

    Solved Java_Currency_Formatter

---
+++ Java_Currency_Formatter.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.text.*;
+
+public class Java_Currency_Formatter {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("US: "+nf.format(payment));
+        nf = NumberFormat.getCurrencyInstance(new Locale("en","IN"));
+        System.out.println("India: "+nf.format(payment));
+        nf = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        System.out.println("China: "+nf.format(payment));
+        nf = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        System.out.println("France: "+nf.format(payment));
+
+    }
+}
