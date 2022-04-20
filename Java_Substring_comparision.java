03808cc9853612964f08386175ae78480ec03d2f
rajasekhar1404 <mrrajasekhar09@gmail.com>
2022-04-20 23:54:42 +0530

    Java_Substring_comparision solved

---
+++ Java_Substring_comparision.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Java_Substring_comparision  {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan=new Scanner(System.in);
+        String str=scan.next();
+        int k=scan.nextInt();
+        SortedSet<String> sets=new TreeSet<String>();
+        for(int i=0;i<=str.length()-k;i++){
+            sets.add(str.substring(i,i+k));
+        }
+        System.out.println(sets.first());
+        System.out.println(sets.last());
+    }
+}
